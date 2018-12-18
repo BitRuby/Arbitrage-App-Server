d3a0f763e6daf9290b4b5f0c8e1d11d4f5f7a652
Michał Zwierzyński <firewallosiem@gmail.com>
2018-12-18 18:02:23 +0100

    header update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(8000, () => {
     console.log('Server started on localhost:8000');
 });
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
